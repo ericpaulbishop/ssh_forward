6604c519da49c03bd6f63fe980ea8583e2cc8a47
Eric <ericpaulbishop@gmail.com>
2013-08-04 22:04:07 -0400

    simplify install

--- debian_install.sh
+++ debian_install.sh
@@ -5,9 +5,7 @@ mkdir -p /etc/ssh_forward
 cp ./ssh_forward.conf /etc/ssh_forward/ssh_forward.conf
 cp ./ssh_forward.init /etc/init.d/ssh_forward
 chmod 755 /etc/init.d/ssh_forward
-for rcn in 2 3 4 5 ; do
-	ln -s /etc/init.d/ssh_forward /etc/rc${rcn}.d/S99ssh_forward
-done
+update-rc.d ssh_forward defaults
 update-rc.d ssh_forward enable
 
-
+/etc/init.d/ssh_forward start
